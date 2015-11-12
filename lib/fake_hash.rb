1407ebe0c5c9ce74a34528c8824ed48ce84f3354
maiya777 <maiya777@list.ru>
2015-11-12 17:12:52 +0300

    Done

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -1,5 +1,44 @@
 class FakeHashWrapper
 
-  # write code here
+  def initialize
+    @hash = {}
+  end
+
+  def []=(key, value)
+      @hash[key] = value
+  end
+
+  def [](key)
+    unless @hash.include?(key)
+      if key.class == Symbol
+      @hash[key.to_s]
+      else
+      @hash[key.to_sym]
+      end
+    else
+      @hash[key]
+    end
+
+  end
+
+  def keys
+    @hash.keys.sort.reverse
+  end
+
+  def first_letter(letter)
+    arr = []
+    @hash.each do |k, v| if k[0] == letter
+      arr << v
+      end
+    end
+      arr
+  end
+
+  def each
+    @hash.each {|k, v|
+      yield k, v
+    }
+
+  end
 
 end
