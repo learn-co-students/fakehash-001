ee9f47217da5d47aa4c0b98156f4a1d588acfcdb
epang1 <epang1@gmail.com>
2016-03-20 20:37:56 -0400

    done

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -1,5 +1,48 @@
+require 'pry'
 class FakeHashWrapper
 
-  # write code here
+def initialize
+  @fh = {}
+  return @fh
+end
+
+def []=(key, value)
+  @fh[key] = value
+end
+
+def [](key)
+  if @fh[key] == nil && key.class == Symbol
+    @fh[key.to_s]
+  elsif @fh[key] == nil && key.class == String
+    @fh[key.to_sym]
+  else
+    @fh[key]
+  end
+end
+
+def keys
+  @fh.keys.sort.reverse
+end
+
+def first_letter(letter)
+  first_letter_array = []
+  fh_keys = []
+  fh_keys = @fh.keys.keep_if {|key| key[0] == letter}
+  fh_keys.each do |key|
+    first_letter_array << @fh[key]
+  end
+
+  first_letter_array  
+
+end
+
+def each
+
+  @fh.each do |key, value|
+    yield(key, value)
+  end
+
+end
+
 
 end
