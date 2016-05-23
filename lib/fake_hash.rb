dc9ec21b29aa3b95c960ede4be1048bd0bd492dd
fhernandez173 <f.hernandez173@gmail.com>
2016-05-23 15:23:43 -0400

    Used require_relative for irb. methods []= and []

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -1,5 +1,52 @@
+require 'pry'
+
+# Used require_relative. methods []= and []
+
 class FakeHashWrapper
 
-  # write code here
+  attr_accessor :fake_hash
+
+  def initialize
+    # binding.pry
+    @fake_hash = {}
+  end
+
+  # setter
+  def []=(key, value)
+    # binding.pry
+    @fake_hash[key] = value
+    value
+  end
+
+  # getter, checking if key is string/ symbol
+  def [](key)
+    # binding.pry
+     @fake_hash[key.to_s] || @fake_hash[key.to_sym]
+  end
+
+  def keys
+    all_the_keys = []
+    @fake_hash.each do |key, value|
+      all_the_keys << key
+    end
+    all_the_keys.sort.reverse
+  end
+
+  def first_letter(letter)
+    # binding.pry
+    all_start_with_a = []
+    @fake_hash.each do |key, value|
+      if key[0] == letter
+        all_start_with_a << value
+      end
+    end
+    all_start_with_a
+  end
+
+  def each
+    self.fake_hash.each do |key, value|
+      puts value
+    end
+  end
 
 end
