d67ce0e8092b547853ba3c252d6c9ce00afa5df4
andrewkjankowski <andrewkjankowski@gmail.com>
2018-01-26 14:13:47 -0500

    Done.

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -1,5 +1,48 @@
+require 'pry'
+
 class FakeHashWrapper
 
+  attr_accessor :k, :v
   # write code here
+  def initialize
+    @k = []
+    @v = []
+  end
 
+  def []=(k, v)
+    if @k.find_index(k.to_sym) != nil
+      @v[@k.find_index(k.to_sym)] = v
+    else
+      @k.push(k.to_sym)
+      @v.push(v)
+    end
+  end
+
+  def [](k)
+    if @k.find_index(k.to_sym) != nil
+      @v[@k.find_index(k.to_sym)]
+    else
+      nil
+    end
+  end
+
+  def keys
+    @k.sort{ |x,y| y <=> x }
+  end
+
+  def first_letter(letter)
+    vals = []
+    @k.each_with_index do |k, index|
+      if k.to_s.split('').first == letter
+        vals << @v[index]
+      end
+    end
+    vals
+  end
+
+  def each(&block)
+    @k.each_with_index do |k, index|
+      block.call(k, @v[index])
+    end
+  end
 end
